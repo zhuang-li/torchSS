0b8ccb0bb23f2f8f948a5411800c1607c44ba124
deathlee <lizhuang144@gmail.com>
2016-08-19 18:56:55 +0800

    update

--- preprocess_glove.sh
+++ preprocess_glove.sh
@@ -3,8 +3,8 @@
 #python2.7 scripts/download.py
 
 glove_dir="data/embedding"
-glove_pre="glove.6B"
-glove_dim="300d"
+glove_pre="glove.twitter.27B"
+glove_dim="200d"
 if [ ! -f $glove_dir/$glove_pre.$glove_dim.th ]; then
     th scripts/convert-wordvecs.lua $glove_dir/$glove_pre.$glove_dim.txt \
         $glove_dir/$glove_pre.vocab $glove_dir/$glove_pre.$glove_dim.th
